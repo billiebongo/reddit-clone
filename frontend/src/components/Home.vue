f71ec4881838bedf3fc52fae4e40170d64ee2a5c
Seng Wee <sengwee_new@hotmail.com>
2019-03-13 23:50:38 +0800

    setting up semantic ui

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <div class="ui container">
+            <h1>Title</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+    }
+</script>
+
+<style scoped>
+
+</style>
