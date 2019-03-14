b882764e2e3b59f6210130daafd99a60f7869ea8
Seng Wee <>
2019-03-14 15:56:12 +0800

    draft sign in page

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,5 +1,7 @@
 import Home from './components/Home.vue';
+import SignIn from './components/SignIn.vue';
 
 export const routes = [
-    { path: "/", component: Home}
+    {path: "/", component: Home},
+    {path: "/signin", component: SignIn}
 ];
